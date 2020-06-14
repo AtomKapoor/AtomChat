f2bb1ad2f59174c390c143c52512c9892c2752d1
Atom Kapoor <AtomKapoor@users.noreply.github.com>
2020-06-15 05:27:56 +0530

    Large upload

---
+++ AtomChat/app/src/main/java/com/atomkapoor/atomchat/StatusActivity.java
@@ -0,0 +1,96 @@
+package com.atomkapoor.atomchat;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class StatusActivity extends AppCompatActivity {
+    private Toolbar mToolbar;
+
+    private EditText mStatus;
+    private Button mSavebtn;
+
+
+    //Firebase
+    private DatabaseReference mStatusDatabase;
+    private FirebaseUser mCurrentUser;
+
+
+    //Progress
+    private ProgressDialog mProgress;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_status);
+
+        //Firebase
+        mCurrentUser = FirebaseAuth.getInstance().getCurrentUser();
+        String current_uid = mCurrentUser.getUid();
+
+        mStatusDatabase = FirebaseDatabase.getInstance().getReference().child("Users").child(current_uid);
+
+        mToolbar = (Toolbar) findViewById(R.id.status_appBar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("Account Status");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        String status_value = getIntent().getStringExtra("status_value");
+
+        mStatus = (EditText) findViewById(R.id.status_input);
+        mSavebtn = (Button) findViewById(R.id.status_save_btn);
+
+        mStatus.setText(status_value);
+
+        mSavebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Progress
+                mProgress = new ProgressDialog(StatusActivity.this);
+                mProgress.setTitle("Saving Changes");
+                mProgress.setMessage("Please wait while we save the changes");
+                mProgress.show();
+
+                String status = mStatus.getText().toString();
+
+                mStatusDatabase.child("status").setValue(status).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+
+                        if(task.isSuccessful()){
+
+                            mProgress.dismiss();
+
+                        } else {
+
+                            Toast.makeText(getApplicationContext(), "There was some error in saving Changes.", Toast.LENGTH_LONG).show();
+
+                        }
+
+                    }
+                });
+
+            }
+        });
+
+
+
+    }
+}
---
+++ AtomChat/app/src/main/java/com/atomkapoor/atomchat/UsersActivity.java
@@ -0,0 +1,144 @@
+package com.atomkapoor.atomchat;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class UsersActivity extends AppCompatActivity {
+    private Toolbar mToolbar;
+
+    private RecyclerView mUsersList;
+
+    private DatabaseReference mUsersDatabase;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_users);
+
+        mToolbar = (Toolbar) findViewById(R.id.users_appBar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("All Users");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mUsersDatabase = FirebaseDatabase.getInstance().getReference().child("Users");
+
+        mUsersList = (RecyclerView) findViewById(R.id.users_list);
+        mUsersList.setHasFixedSize(true);
+        mUsersList.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseRecyclerOptions<Users> options =
+                new FirebaseRecyclerOptions.Builder<Users>()
+                        .setQuery(mUsersDatabase, Users.class)
+                        .setLifecycleOwner(this)
+                        .build();
+
+        FirebaseRecyclerAdapter firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<Users, UsersViewHolder>(options) {
+
+            @Override
+            public UsersViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.users_single_layout, parent, false);
+
+                return new UsersViewHolder(view);
+            }
+
+            @Override
+            protected void onBindViewHolder(UsersViewHolder usersViewHolder, int position, Users users) {
+                // Bind the users object to the BlogViewHolder
+                usersViewHolder.setDisplayName(users.getName());
+                usersViewHolder.setUserStatus(users.getStatus());
+                usersViewHolder.setUserImage(users.getThumb_image(), getApplicationContext());
+
+                final String user_id = getRef(position).getKey();
+
+                usersViewHolder.mView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                        Intent profileIntent = new Intent(UsersActivity.this, ProfileActivity.class);
+                        profileIntent.putExtra("user_id", user_id);
+                        startActivity(profileIntent);
+
+                    }
+                });
+
+
+            }
+        };
+        mUsersList.setAdapter(firebaseRecyclerAdapter);
+        }
+
+
+        public static class UsersViewHolder extends RecyclerView.ViewHolder {
+
+        View mView;
+
+        public UsersViewHolder(View itemView) {
+            super(itemView);
+
+            mView = itemView;
+
+        }
+
+        public void setDisplayName(String name){
+
+            TextView userNameView = (TextView) mView.findViewById(R.id.user_single_name);
+            userNameView.setText(name);
+
+        }
+
+        public void setUserStatus(String status){
+
+            TextView userStatusView = (TextView) mView.findViewById(R.id.user_single_status);
+            userStatusView.setText(status);
+
+
+        }
+
+        public void setUserImage(String thumb_image, Context ctx){
+
+            CircleImageView userImageView = (CircleImageView) mView.findViewById(R.id.user_single_image);
+
+            Picasso.with(ctx).load(thumb_image).placeholder(R.drawable.default_avatar).into(userImageView);
+
+        }
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
